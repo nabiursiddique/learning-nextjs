a6f008c444449434c1ba53492a118d56dc10f4cd
nabiursiddique <nabiursiddique01@gmail.com>
2023-09-10 22:35:26 +0600

    Learned custom font uses

--- src/app/photos/page.jsx
+++ src/app/photos/page.jsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 import localImage from '../../../public/next.svg'
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+    weight: '700',
+    subsets: ['latin']
+});
 
 const photos = () => {
     return (
@@ -7,11 +13,12 @@ const photos = () => {
             <h5>Image from local file</h5>
             <Image src={localImage} alt='Image' />
             <br />
-            <h5>Image form online sources</h5>
+            <h5 className={roboto.className}>Image form online sources</h5>
             <Image
+                className='resize'
                 src={'https://images.pexels.com/photos/10671403/pexels-photo-10671403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'}
-                width={1000}
-                height={1000}
+                width={500}
+                height={500}
                 alt='internet Image' />
         </div>
     );
